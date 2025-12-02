d269061e92c9fded8ab9d8b7e27e2bec4c16c814
fabiankurzeja <fabian.kurzeja@web.de>
2025-12-02 20:43:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Test Project to Publish a Project to Maven
---
+++ src/main/java/com/ediconverter/some/test/Test.java
@@ -0,0 +1,7 @@
+package com.ediconverter.some.test;
+
+public class Test {
+    public int rocketLaunch(int a, int b){
+        return a+b;
+    }
+}
